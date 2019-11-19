d71b519926ae9372678622228ec9309fc2e986d1
Bruno Paulino <bruno@woombikes.com>
2019-11-19 16:26:12 +0100

    add security group

--- main.tf
+++ main.tf
@@ -2,7 +2,28 @@ provider "aws" {
     region = "eu-central-1"
 }
 
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_instance" "example" {
-    ami = "ami-09a83aee4e05196ae"
-    instance_type = "t2.micro"
+    ami                     = "ami-09a83aee4e05196ae"
+    instance_type           = "t2.micro"
+    vpc_security_group_ids  = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "hello, world" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    tags = {
+        Name = "terraform-example"
+    }
 }
