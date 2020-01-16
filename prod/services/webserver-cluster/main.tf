7e021dfa6ea3b661facb563b85d9fb906c01adb0
Bruno Paulino <bruno@woombikes.com>
2020-01-16 16:54:37 +0100

    using module output variables

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -9,3 +9,25 @@ module "webserver_cluster" {
   db_remote_state_bucket = "terraform-up-and-running-state-bruno"
   db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
 }
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 10
+  recurrence            = "0 9 * * *"
+
+  # Modules can also make output variables available
+  # check outputs.tf on the module folder
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
